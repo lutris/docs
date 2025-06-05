2b701818d30c360371444d45bc69803da38d5d08
Twig6943 <119701717+Twig6943@users.noreply.github.com>
2025-06-05 22:17:56 +0300

    Delete scripts directory

--- scripts/DisableNetwork.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-
-# Remove `/usr/bin/flatpak-spawn --host` if not using Flatpak
-/usr/bin/flatpak-spawn --host bash -c '
-echo "Disabling all networking via NetworkManager..."
-nmcli networking off
-'
